9ea45cc149c0ad9713d774359566738576dc2e6d
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2022-04-09 04:25:27 +0000

    Tweaking model logging.

--- hateful_memes/utils.py
+++ hateful_memes/utils.py
@@ -3,7 +3,7 @@ from pytorch_lightning.loggers import WandbLogger, CSVLogger
 def get_project_logger(*, project=None, save_dir=None, offline=False):
     """ Creates a logger for the project."""
     return [
-        WandbLogger(project=project, offline=offline, log_model=True),
+        WandbLogger(project=project, offline=offline, log_model=not offline),
         CSVLogger(save_dir=save_dir)
     ]
 
