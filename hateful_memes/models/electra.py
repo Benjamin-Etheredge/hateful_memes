d6aef421fe64d4bd640fae5e7662ec74573f93f1
johnweichow <johnweichow@gmail.com>
2022-04-13 10:26:39 -0400

    building out model

---
+++ hateful_memes/models/electra.py
@@ -0,0 +1,101 @@
+import pytorch_lightning as pl
+import torch
+import click
+from transformers import ElectraTokenizerFast, ElectraModel
+import torchvision.models as models
+from hateful_memes.data.hateful_memes import MaeMaeDataModule
+from torch.nn import functional as F
+from torch import nn
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger
+from icecream import ic
+
+
+class ElectraModule(pl.LightningModule):
+
+    def __init__(
+        self, 
+        lr=0.003, 
+        max_length=512, 
+        include_top=True,
+        dropout_rate=0.0,
+        dense_dim=256,
+    ):
+        super().__init__()
+        self.ElectraModel = ElectraModel.from_pretrained("google/electra-small-discriminator")
+        ic(self.ElectraModel)
+        self.tokenizer = ElectraTokenizerFast.from_pretrained("google/electra-small-discriminator")
+        ic(self.tokenizer)
+
+        self.fc1 = nn.Linear(768, dense_dim)
+        self.fc2 = nn.Linear(dense_dim, 1)
+
+        self.lr = lr
+        self.max_length = max_length
+        self.include_top = include_top
+        self.dropout_rate = dropout_rate
+        self.dense_dim = dense_dim
+
+    def _shared_step(self, batch):
+        y_hat = self.forward(batch)
+        y = batch['label']
+        loss = F.binary_cross_entropy_with_logits(y_hat, y.to(y_hat.dtype))
+        acc = torch.sum(torch.round(torch.sigmoid(yhat)) == y.data) / (y.shape[0] * 1.0)
+        return loss, acc
+
+    def validation_step(self, batch):
+        loss, acc = self._shared_step(batch)
+        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch['image'].size(0))
+        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch['image'].size(0))
+        return loss
+    
+    def training_step(self, batch):
+        loss, acc = self._shared_step(batch)
+        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch['image'].size(0))
+        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, batch_size=batch['image'].size(0))
+        return loss
+    
+    def forward(self, batch):
+        text = batch['text']
+        inputs = self.tokenizer(
+            text,
+            return_tensors='pt',
+            padding='max_length',
+            truncation=True,
+            max_length=self.max_length,
+        )
+        inputs = inputs.to(self.device)
+
+        x = self.ElectraModel(inputs)
+
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout_rate)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout_rate)
+        x.squeeze_()
+        return x
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+    
+    
+
+
+@click.command()
+@click.option('--batch_size',          default=32,     help='Batch size')
+@click.option('--lr',                  default=1e-4,   help='Learning rate')
+@click.option('--max_length',          default=128,    help='Max length')
+@click.option('--dense_dim',           default=256,    help='Dense dim')
+@click.option('--dropout_rate',        default=0.1,    help='Dropout rate')
+@click.option('--epochs',              default=10,     help='Epochs')
+@click.option('--model_dir',           default='/tmp', help='Save dir')
+@click.option('--gradient_clip_value', default=1.0,    help='Gradient clip')
+@click.option('--fast_dev_run',        default=False,  help='Fast dev run')
+
+def main():
+    logger = None if fast_dev_run else WandbLogger(project="electra")
+    
