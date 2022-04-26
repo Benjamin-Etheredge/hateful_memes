2363f98449192cc77adbb099e1e8f5063a4a1445
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2022-04-26 21:31:46 +0000

    Fixing merge conflicts.

--- hateful_memes/models/visual_bert_with_od.py
+++ hateful_memes/models/visual_bert_with_od.py
@@ -206,18 +206,18 @@ class VisualBertWithODModule(BaseMaeMaeModel):
         ############################################
         # Obj Detection Start
         ############################################
-        # self.od_config = AutoConfig.from_pretrained('facebook/detr-resnet-50', num_queries=num_queries)
-        # self.od_feature_extractor = DetrFeatureExtractor.from_pretrained('facebook/detr-resnet-50')
-        # self.od_model = DetrForObjectDetection(self.od_config)
-        self.transform = torchvision.transforms.ToPILImage()
-
-        # for param in self.od_model.parameters():
-        #     param.requires_grad = False
-        # self.od_model.eval()
-
-        # # ic(self.od_model)
+        self.od_config = AutoConfig.from_pretrained('facebook/detr-resnet-50', num_queries=num_queries)
+        self.od_feature_extractor = DetrFeatureExtractor.from_pretrained('facebook/detr-resnet-50')
+        self.od_model = DetrForObjectDetection(self.od_config)
+        # ic(self.od_config.num_queries)
         # self.od_fc = nn.Linear(self.od_config.num_queries * 256, 2048)
-        # # self.od_fc = nn.Linear(256, 768)
+        # self.od_fc = nn.Sequential(
+        #     nn.Linear(256, 2048),
+        #     # nn.BatchNorm2d(2048),
+        #     nn.Tanh(),
+        #     # nn.AdaptiveAvgPool1d(1)
+        # )
+        # self.od_fc = nn.Linear(256, 768)
 
         self.detr2 = Detectron2Module(batch_size=batch_size, num_queries=num_queries)
          
@@ -246,6 +246,7 @@ class VisualBertWithODModule(BaseMaeMaeModel):
         self.visual_bert_config = self.visual_bert.config
         self.last_hidden_size = dense_dim
         self.num_queries = num_queries
+        # self.image_transformer = T.ToPILImage()
 
         self.save_hyperparameters()
     
@@ -282,6 +283,25 @@ class VisualBertWithODModule(BaseMaeMaeModel):
         # ic()
         image_x = self.detr2.get_visual_embeds(images_list)
         # ic()
+        # images_list = [self.image_transformer(x_) for x_ in image]
+
+        # od_inputs = self.od_feature_extractor(images=images_list, return_tensors="pt")
+        # od_inputs = od_inputs.to(self.device)
+        # od_outputs = self.od_model(**od_inputs)
+        # # ic(od_outputs.keys())
+        # # for key in od_outputs.keys():
+        # #     ic(key, od_outputs[key].shape)
+
+        # image_x = od_outputs.last_hidden_state
+        # # ic(image_x.shape)
+
+        # # image_x = image_x.view(image_x.shape[0], 1, -1)
+        # image_x = self.od_fc(image_x)
+        # image_x = image_x.permute(0, 2, 1)
+        # image_x = F.adaptive_avg_pool1d(image_x, 1)
+        # image_x = image_x.permute(0, 2, 1)
+        # # image_x = torch.squeeze(image_x, dim=-1)
+        # image_x = torch.tanh(image_x)
         ############################################
         # Obj Detection End
         ############################################
