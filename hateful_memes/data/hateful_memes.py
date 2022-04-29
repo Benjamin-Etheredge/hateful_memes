73c360a2560d8b3e1577d0f0d72f7a6914a39820
Benjamin Etheredge <benjamin.etheredge@gmail.com>
2022-04-29 01:08:09 +0000

    Setting up different train/val transformers.

--- hateful_memes/data/hateful_memes.py
+++ hateful_memes/data/hateful_memes.py
@@ -51,7 +51,10 @@ class MaeMaeDataset(torch.utils.data.Dataset):
         # if self.txt_transforms is None:
         #     self.txt_transforms = self.create_text_transform()
         if self.img_transforms is None:
-            self.img_transforms = self.base_img_transforms()
+            if set == "train":
+                self.img_transforms = self.base_train_img_transforms()
+            else:
+                self.img_transforms = self.base_test_img_transforms()
 
         self.include_text_features = include_text_features
         self.vocab = None
@@ -89,7 +92,7 @@ class MaeMaeDataset(torch.utils.data.Dataset):
         return sample
 
     # TODO vocab is broken between train and test
-    def base_img_transforms(self):
+    def base_train_img_transforms(self):
         return T.Compose([
             # T.RandomHorizontalFlip(),
             # transforms.ToPILImage(mode='RGB'),
@@ -99,24 +102,15 @@ class MaeMaeDataset(torch.utils.data.Dataset):
                                   std=[0.229, 0.224, 0.225]),
         ])
 
-
-    def create_text_transform(self):
-        # TODO maybe move into dataset
-        from torchtext.data.utils import get_tokenizer
-        from torchtext.vocab import build_vocab_from_iterator
-
-        self.tokenizer = get_tokenizer('basic_english')
-        def yield_tokens(data_iter):
-            pool = Pool(processes=os.cpu_count())
-            # return pool.map(self.tokenizer, [item['text'] for item in data_iter])
-            return pool.map(self.tokenizer, self.info['text'])
-            # for item in data_iter:
-            #     # ic(item)
-            #     yield tokenizer(item['text'])
-        self.vocab = build_vocab_from_iterator(yield_tokens(None), specials=["<unk>"])
-        self.vocab.set_default_index(self.vocab["<unk>"])
-
-        return lambda x: self.vocab(self.tokenizer(x))
+    def base_test_img_transforms(self):
+        return T.Compose([
+            # T.RandomHorizontalFlip(),
+            # transforms.ToPILImage(mode='RGB'),
+            T.Resize(size=(224,224)),
+            T.ToTensor(), # this already seems to scale okay
+            T.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225]),
+        ])
 
 class MaeMaeDataModule(pl.LightningDataModule):
     def __init__(
@@ -133,7 +127,6 @@ class MaeMaeDataModule(pl.LightningDataModule):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
-        # self.img_transforms = img_transforms
         self.img_transforms = img_transforms
         self.txt_transforms = txt_transforms
 
@@ -172,13 +165,6 @@ class MaeMaeDataModule(pl.LightningDataModule):
             set="test_seen",
         )
 
-    
-    # def create_collate_fn(self):
-        # if self.collate_fn is None:
-        #     return default_collate
-        # else:
-        #     return self.collate_fn
-
     def train_dataloader(self, shuffle=True, drop_last=True):
         return torch.utils.data.DataLoader(
             self.train_dataset,
@@ -188,7 +174,6 @@ class MaeMaeDataModule(pl.LightningDataModule):
             pin_memory=self.pin_memory,
             drop_last=drop_last,
             persistent_workers=False,
-            # collate_fn=MaeMaeDataset.collate_batch,
         )
 
     def val_dataloader(self):
@@ -199,8 +184,6 @@ class MaeMaeDataModule(pl.LightningDataModule):
             num_workers=self.num_workers,
             pin_memory=self.pin_memory,
             persistent_workers=False,
-            # collate_fn=MaeMaeDataset.collate_batch,
-            # collate_fn=self.collate_batch, = None
         )
     
     def test_dataloader(self):
@@ -210,44 +193,5 @@ class MaeMaeDataModule(pl.LightningDataModule):
             shuffle=False,
             num_workers=self.test_num_workers,
             pin_memory=self.pin_memory,
-            # collate_fn=MaeMaeDataset.collate_batch,
-            # collate_fn=self.collate_batch,
         ) 
-
-    # def collate_batch(self, batch):
-    #     img_list, labels_list, raw_text, text_list, offsets = [], [], [], [], [0]
-    #     # ic(batch)
-    #     # if type(batch) == list:
-    #         # ic(batch)
-    #     for item in batch:
-    #         img_list.append(item['image'])
-    #         labels_list.append(item['label'])
-
-    #         _text = item['text']
-    #         processed_text = torch.tensor(self.text_pipeline(_text), dtype=torch.int64)
-    #         raw_text.append(_text)
-    #         text_list.append(processed_text)
-    #         offsets.append(processed_text.size(0))
-
-    #     img_list = torch.torch(img_list)
-        
-    #     labels_list = torch.tensor(labels_list, dtype=torch.int32)
-    #     offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
-    #     text_list = torch.cat(text_list)
-    #     return dict(
-    #         text_features=text_list,
-    #         text_offset=offsets,
-    #         label=labels_list,
-    #         image=img_list,
-    #         text=raw_text,
-    #     )
-            
-# def create_transformer(img_transforms=None, text_transforms=None):
-#     def wrapper_transformer(sample: Dict):
-#         if img_transforms:
-#             sample['image'] = img_transforms(sample['image'])
-#         if text_transforms:
-#             sample['text'] = text_transforms(sample['text'])
-#         return sample
-#     return wrapper_transformer
 
