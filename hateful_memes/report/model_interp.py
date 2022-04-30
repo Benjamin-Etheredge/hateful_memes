e28de079b054b66bace82e94d242afeec775b8de
Marcus Thoreson <mthoreson3@gatech.edu>
2022-04-30 06:51:48 +0000

    Text and visual feature attribution figures working for visual-bert and electra

--- hateful_memes/report/model_interp.py
+++ hateful_memes/report/model_interp.py
@@ -47,17 +47,17 @@ class InterpModel():
             raise ValueError
         elif model_name == 'visual-bert':
             self.inner_model = VisualBertModule.load_from_checkpoint(checkpoint_path=ckpt_path)
-            self.inner_model.eval()
             self.image_embed_layer = self.inner_model.resnet
             self.text_embed_layer = self.inner_model.visual_bert.embeddings.word_embeddings
             self.tokenizer = self.inner_model.tokenizer
         elif model_name == 'beit':
             self.inner_model = BaseITModule.load_from_checkpoint(checkpoint_path=ckpt_path)
-            self.inner_model.eval()
-            self.image_embed_layer = self.inner_model.feature_extractor
+            in_wrap = ModelInputWrapper(self.inner_model.model.beit)
+            self.inner_model.model.beit = in_wrap
+            #self.image_embed_layer = self.inner_model.model.beit.embeddings.patch_embeddings
+            self.image_embed_layer = in_wrap.input_maps["pixel_values"]
         elif model_name == 'electra':
-            self.inner_model == AutoTextModule.load_from_checkpoint(checkpoint_path=ckpt_path)
-            self.inner_model.eval()
+            self.inner_model = AutoTextModule.load_from_checkpoint(checkpoint_path=ckpt_path)
             self.text_embed_layer = self.inner_model.model.embeddings.word_embeddings
             self.tokenizer = self.inner_model.tokenizer
             
@@ -74,24 +74,6 @@ class InterpModel():
         #ic(batch)
         return self.inner_model(batch)
     
-
-def wrapper_forward(image, input_ids, tokenizer, pl_module):
-    #ic(image)
-    #ic(input_ids)
-    #ic(tokenizer_inner_model)
-    #tokenizer=tokenizer_pl_module[0]
-    #pl_module=tokenizer_pl_module[1]
-    # reassemble dict
-    text_orig = [tokenizer.decode(input_id, skip_special_tokens=True)
-        for input_id in input_ids.tolist()]
-    #ic(text_orig)
-    batch = {
-        'image': image,
-        'text': text_orig
-    }
-    #ic(batch)
-    return pl_module(batch)
-
 
 def get_attributions(interp_model:InterpModel, data_sample):
     ## Calculate feature attribution
@@ -112,6 +94,7 @@ def get_attributions(interp_model:InterpModel, data_sample):
     text_baselines = token_ref.generate_reference(token_ids.numel(), device='cpu').unsqueeze(0)
     # Layer Integrated Gradients
     attrs = {}
+    interp_model.inner_model.eval()
     if interp_model.image_embed_layer is not None:
         lig_image = LayerIntegratedGradients(interp_model, interp_model.image_embed_layer)
         img_attr = lig_image.attribute(inputs=image_text,
@@ -129,21 +112,72 @@ def get_attributions(interp_model:InterpModel, data_sample):
     return attrs
 
 
-def visualize_attributions(attrs, inputs, y_hat, y_label, tokenizer,
-    save_results=False, save_dir="data/08_reporting", save_name="tmp.png"):
+def visualize_attributions(attrs, inputs, y_hat, y, tokenizer, model_name,
+    save_dir="data/08_reporting", save_name="tmp.png"):
+    
+    # Prediction string
+    pred = 1 if y_hat>0.5 else 0
+    y_hat_label = "Hateful" if pred==1 else "Not Hateful" 
+    y_label = "Hateful" if y==1 else "Not Hateful" 
+    pn_str = "positive"
+    tf_str = "false"
+    if y==pred:
+        tf_str = "true"
+    if pred==0:
+        pn_str = "negative"
+    pred_str = "Model predicted: \"%s\", probability %.3g\n(%s %s)" % (y_hat_label, y_hat, tf_str, pn_str)
+
+    # Both visualizations use the original image
+    img_in = inputs['img']
+    img_in_vis = img_in.squeeze(dim=0).permute(1, 2, 0).numpy()
+
+    # Set figure up 
+    num_subs = 1
+    width_ratios=[5]
+    if 'img' in attrs.keys():
+        num_subs += 1
+        width_ratios.append(6)
+    if 'txt' in attrs.keys():
+        num_subs += 3
+        width_ratios.append(1.5)
+        width_ratios.append(2)
+        width_ratios.append(1.5)
+    if 'txt' in attrs.keys() and 'img' in attrs.keys():
+        num_subs += 3
+        width_ratios.append(1.5)
+        width_ratios.append(2)
+        width_ratios.append(1.5)        
+    fig = plt.figure(figsize=(12, 5), constrained_layout=False)
+    gs = fig.add_gridspec(1, num_subs, left=0.025, right=1.0, bottom=0.1, top=0.75, wspace=0.1, width_ratios=width_ratios)
+    ax_orig = fig.add_subplot(gs[0, 0])
+    ax_orig.imshow(img_in_vis)
+    ax_orig.xaxis.set_ticks_position("none")
+    ax_orig.yaxis.set_ticks_position("none")
+    ax_orig.set_xticklabels([])
+    ax_orig.set_yticklabels([])
+    ax_orig.set_title("Original")
+    current_sub = 1
+
+    img_attr = None
+    txt_attr = None
+    # Visualize image feature attributions
     if 'img' in attrs.keys():
         img_attr = attrs['img']
-        img_in = inputs['img']
         img_attr_vis = img_attr.squeeze(dim=0).permute(1, 2, 0).numpy()
-        img_in_vis = img_in.squeeze(dim=0).permute(1, 2, 0).numpy()
-        fig, ax = visualization.visualize_image_attr(img_attr_vis, original_image=img_in_vis,
-            method="blended_heat_map", sign="all", show_colorbar=True)
-        if(save_results):
-           fig.savefig(os.path.join(save_dir, save_name))
-
+        axi = fig.add_subplot(gs[0, current_sub])
+        current_sub += 1
+        img_attr_comb = np.sum(img_attr_vis, axis=-1)
+        img_attr_normed = img_attr_comb/np.linalg.norm(img_attr_comb)
+        imi = axi.imshow(img_attr_normed)
+        axi.xaxis.set_ticks_position("none")
+        axi.yaxis.set_ticks_position("none")
+        axi.set_xticklabels([])
+        axi.set_yticklabels([])        
+        axi.set_title("Norm. Visual Attr.")
+        fig.colorbar(imi, ax=axi)
+    
+    # Visualize text feature attributions
     if 'txt' in attrs.keys():
-        # Look at https://captum.ai/tutorials/Multimodal_VQA_Interpret
-        # something = visualization.VisualizationDataRecord(...)
         txt_attr = attrs['txt'].squeeze(0)  # Should now be (max_length x embed_dim)
         txt_in = inputs['txt']
         token_dict = tokenizer(
@@ -154,65 +188,32 @@ def visualize_attributions(attrs, inputs, y_hat, y_label, tokenizer,
             max_length=txt_attr.shape[0],
             add_special_tokens=True, return_special_tokens_mask=True)
         
-        ic(txt_in)
-
         token_mask = token_dict['special_tokens_mask'] < 1
         txt_attr = txt_attr.sum(-1)
         txt_attr = torch.masked_select(txt_attr, token_mask.squeeze(0))
         txt_attr_norm = txt_attr.norm()
         txt_attr_normed = txt_attr/txt_attr_norm
-        y_hat_label = "Hateful" if y_hat>0.5 else "Not Hateful" 
-
-
-        fig, ax = plt.subplots()
-        txt_img = ax.imshow(txt_attr_normed.unsqueeze(-1).numpy())
-        ax.set_yticks(np.arange(txt_attr_normed.numel()), labels=txt_in[0].split())
+        
+        axt = fig.add_subplot(gs[0, current_sub+1])
+        current_sub += 3
+        imt = axt.imshow(txt_attr_normed.unsqueeze(-1).numpy())
+        axt.set_yticks(np.arange(txt_attr_normed.numel()), labels=txt_in[0].split())
         for idx, attr_i in enumerate(txt_attr_normed.tolist()):
-            ax.text(0, idx, '%.3g' % (attr_i,), ha="center", va="center", color="w")
-        ax.figure.colorbar(txt_img, ax=ax)
-        ax.axes.xaxis.set_ticklabels([])
-        fig.tight_layout()
-        fig.savefig(os.path.join(save_dir, "tmp_txt.png"))
-        """
-        junk = Image.new("RGB", (1,1))
-        draw = ImageDraw.Draw(junk)
-        width, height = draw.textsize(text=txt_in)
-        mline_text = txt_in[0]
-        width, height = draw.multiline_textsize(text=mline_text)
-        spacing = 20
-        text_vis = Image.new("RGB", ((2*spacing) + width, (2*spacing) + height), (255, 255, 255))
-        text_vis_font = ImageFont.load_default()
-        draw_ctx = ImageDraw.Draw(text_vis)
-        pos_color = (0, 255, 0)
-        neg_color = (255, 0, 0)
-        paste_loc = (spacing, spacing)
-        for idx, word in enumerate(txt_in[0].split()):
-            word_w, word_h = draw_ctx.textsize(word)
-            word_color = (255,255,255)
-            if txt_attr_normed[idx] > 0:
-                word_alpha = round(txt_attr_normed[idx].item() * 255.)
-                word_color = pos_color + (word_alpha,)
-            else:
-                word_alpha = round(abs(txt_attr_normed[idx].item() * 255.))
-                word_color = neg_color + (word_alpha,)
-            word_img = Image.new("RGBA", (word_w, word_h), word_color)
-            text_vis.paste(word_img, paste_loc)
-            #draw_ctx.text(paste_loc, word + "\n", font=text_vis_font)
-            paste_loc = (paste_loc[0], paste_loc[1] + word_h) 
-        draw_ctx.multiline_text(text=mline_text, xy=(spacing,spacing), spacing=4, font=text_vis_font)
-        #vis_rec = visualization.VisualizationDataRecord(txt_attr/txt_attr_norm, y_hat, y_hat_label, y_label, y_hat_label,
-        #    txt_attr.sum(), txt_in[0].split(), 0.0)
-        #html = visualization.visualize_text([vis_rec], legend=True)
+            axt.text(0, idx, '%.2g' % (attr_i,), ha="center", va="center", color="w")
+        fig.colorbar(imt, ax=axt)
+        axt.xaxis.set_ticks_position("none")
+        axt.set_xticklabels([])
+        axt.set_title("Norm. Text Attr.")
 
-        if(save_results):
-            text_vis.save(os.path.join(save_dir, "tmp_txt.png"))
-        #ic(txt_attr.shape)
-        #ic(txt_attr)
-        """
+    if 'txt' in attrs.keys() and 'img' in attrs.keys():
+        total_abs_vis = img_attr.sum().item()
+        total_abs_txt = txt_attr.sum().item()
+        axb = fig.add_subplot(gs[0, current_sub+1])
+        axb.bar(["Visual", "Text"], [total_abs_vis, total_abs_txt])
+        axb.set_title("Net Total Attribution")
 
-    total_attr = img_attr.sum().item() + txt_attr.sum().item()
-    ic("ATTR:", total_attr)
-    ic("PRED:", y_hat)
+    fig.suptitle("%s Attribution Scores\n%s" % (model_name, pred_str))
+    fig.savefig(os.path.join(save_dir, save_name))
        
 
 @click.command
@@ -233,14 +234,15 @@ def interp(model_name:str, ckpt_dir:str, no_save:bool, save_dir:str, save_name:s
     ## Get attributions
     attrs = get_attributions(interp_model, data_sample)
     ## Visualize attributions
-    inputs = {
-        'img':data_sample["image"],
-        'txt':data_sample["text"]
-    }
-    y_hat = interp_model.inner_model(data_sample).item()
-    y_label = "Hateful" if data_sample["label"]>0 else "Not Hateful" 
-    visualize_attributions(attrs, inputs, y_hat, y_label, interp_model.tokenizer,
-        ~no_save, save_dir, save_name)
+    if(~no_save):
+        inputs = {
+            'img':data_sample["image"],
+            'txt':data_sample["text"]
+        }
+        y_hat = interp_model.inner_model(data_sample).item()
+        y = data_sample["label"] 
+        visualize_attributions(attrs, inputs, y_hat, y, interp_model.tokenizer,
+            model_name, save_dir, save_name)
 
 
 if __name__ == '__main__':
